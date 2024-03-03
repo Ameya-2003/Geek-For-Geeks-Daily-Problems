ce53ad4a4bff2cc7e018c98c201fe73706d16bad
Ameya Muktewar <ameyamuktewargithub@gmail.com>
2024-03-03 20:06:56 +0530

    Largest Number formed from an Array.java
    Given an array of strings arr[] of length n representing non-negative integers, arrange them in a manner, such that, after concatanating them in order, it results in the largest possible number. Since the result may be very large, return it as a string.
    
    Example 1:
    
    Input: 
    n = 5
    arr[] =  {"3", "30", "34", "5", "9"}
    Output: "9534330"
    Explanation: 
    Given numbers are  {"3", "30", "34", "5", "9"}, 
    the arrangement "9534330" gives the largest value.
    Example 2:
    
    Input: 
    n = 4
    arr[] =  {"54", "546", "548", "60"}
    Output: "6054854654"
    Explanation: 
    Given numbers are {"54", "546", "548", "60"}, the 
    arrangement "6054854654" gives the largest value.
    Your Task:  
    You don't need to read input or print anything. Your task is to complete the function printLargest() which takes the array of strings arr[] as a parameter and returns a string denoting the answer.
    
    Expected Time Complexity: O(n*log(n) ).
    Expected Auxiliary Space: O(n).
    
    Constraints:
    1 ≤ n ≤ 105
    0 ≤ arr[i] ≤ 1018
    Sum of all the elements of the array is greater than 0.

---
+++ Solutions/Medium/Largest Number formed from an Array.java
@@ -0,0 +1,21 @@
+class Solution {
+ String printLargest(int n, String[] arr) {
+        
+        Comparator<String> com = new Comparator<String>() {
+            public int compare(String st1, String st2) {
+                String s1 = st1 + st2;
+                String s2 = st2 + st1;
+                
+                return s2.compareTo(s1); 
+            }
+        };
+      
+        Arrays.sort(arr, com);
+      
+        StringBuilder result = new StringBuilder();
+        for (String s : arr) {
+            result.append(s);
+        }
+        return result.toString();
+    }
+}
