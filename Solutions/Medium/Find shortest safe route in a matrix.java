dbdbf98623f53a7ff735e11461d86e08b3c314b6
Ameya Muktewar <ameyamuktewargithub@gmail.com>
2024-03-27 11:28:31 +0530

    Find shortest safe route in a matrix.java
    Given a matrix mat[][] with r rows and c columns, where some cells are landmines (marked as 0) and others are safe to traverse. Your task is to find the shortest safe route from any cell in the leftmost column to any cell in the rightmost column of the mat. You cannot move diagonally, and you must avoid both the landmines and their adjacent cells (up, down, left, right), as they are also unsafe.
    
    Example 1:
    
    Input:
    mat = [1, 0, 1, 1, 1],
          [1, 1, 1, 1, 1],
          [1, 1, 1, 1, 1],
          [1, 1, 1, 0, 1],
          [1, 1, 1, 1, 0]
    Output: 
    6
    Explanation: 
    We can see that length of shortest
    safe route is 6
    [1 0 1 1 1]
    [1 1 1 1 1] 
    [1 1 1 1 1]
    [1 1 1 0 1] 
    [1 1 1 1 0]
    Example 2:
    
    Input:
    mat = [1, 1, 1, 1, 1],
          [1, 1, 0, 1, 1],
          [1, 1, 1, 1, 1]
    Output: 
    -1
    Explanation: There is no possible path from
    first column to last column.
    Your Task:
    You don't need to read input or print anything. Your task is to complete the function findShortestPath() which takes matrix as input parameter and return an integer denoting the shortest safe path form any cell in leftmost column to any cell in rightmost column of mat. If there is no possible path, return -1. 
    
    Expected Time Complexity: O(r * c)
    Expected Auxiliary Space: O(r * c)
    
    Constraints:
    1 <= r, c <= 103
    0 <= mat[][] <= 1

---
+++ Solutions/Medium/Find shortest safe route in a matrix.java
@@ -0,0 +1,58 @@
+class Solution {
+    
+    static int[][]dirs = {{1,0},{-1,0},{0,1},{0,-1}}; 
+    
+    public static int findShortestPath(int[][] mat) {
+        
+        int n = mat.length, m = mat[0].length;
+        
+        Queue<int[]>queue = new LinkedList<>();
+        
+        /* leftmost columns check if one and adjacent nodes are not landmines then add in queue */
+        
+        for(int i=0;i<n;i++){
+            if(mat[i][0]==1 && checkLandmines(mat,i,0,n,m))queue.add(new int[]{i,0,0});
+        }
+        
+        /*  traverse all paths from leftmost col to rightmost col using queue and find shortest path */
+        int ans = Integer.MAX_VALUE;
+        while(!queue.isEmpty()){
+            int[]arr = queue.poll();
+            int row = arr[0], col = arr[1], dist = arr[2];
+            
+            /* mark visited */
+            mat[row][col] = -1;
+            
+            /* path valid condition then store minimum */
+            if(col == m-1)ans = Math.min(ans,dist+1);
+            
+            /* traverse all adjacent which is not landmines as well as not visited */
+            for(int[]dir:dirs){
+                int nrow = dir[0]+row;
+                int ncol = dir[1]+col;
+                
+                /* visited and landmine condition */
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && mat[nrow][ncol]!=-1 && checkLandmines(mat,nrow,ncol,n,m)){
+                    queue.add(new int[]{nrow,ncol,dist+1});
+                }
+            }
+        }
+        
+        /* check for invalid case */
+        if(ans == Integer.MAX_VALUE)return -1;
+        
+        return ans;
+        
+    }
+    
+    public static boolean checkLandmines(int[][]mat, int row, int col,int n, int m){
+        for(int[]dir:dirs){
+            int nrow = dir[0]+row;
+            int ncol = dir[1]+col;
+            
+            /*  landmine condition */
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && mat[nrow][ncol]==0)return false;
+        }
+        return true;
+    }
+}
